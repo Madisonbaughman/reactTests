fce82c18cd98b201a3f675260b2555ed974b26d6
Madisonbaughman <89171880+Madisonbaughman@users.noreply.github.com>
2023-04-18 22:03:02 -0400

    breaking changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import NavBar from './navbar';
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Route, Switch} from 'react-router-dom';
 import Project1 from './projectPages/Project1';
 import Project2 from './projectPages/Project2';
 import Project3 from './projectPages/Project3';
@@ -8,30 +8,23 @@ import Project4 from './projectPages/Project4';
 
 function App() {
   return (
-    //<Router>
+    
       <div>
         <NavBar />
         <div className="App">
           <header className="App-header">
             My App
             <Switch>
-              <Route exact path="/">
-                <Project1/>
-              </Route>
-              <Route path="/project2">
-                <Project2/>
-              </Route>
-              <Route path="/project3">
-                <Project3/>
-              </Route>
-              <Route path="/project4">
-                <Project4/>
-              </Route>
+              <Route exact path="/" render={() => <Project1 />} />
+              <Route path="/project2" render={() => <Project2 />} />
+              <Route path="/project3" render={() => <Project3 />} />
+             <Route path="/project4" render={() => <Project4 />} />
+
             </Switch> 
           </header>
         </div>
       </div>
-   // </Router>
+   
   );
 }
 
